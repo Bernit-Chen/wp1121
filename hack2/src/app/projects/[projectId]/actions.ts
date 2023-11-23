63f44f5f958d4e909e30af4390e2ae23ed43159e
Bernit-Chen <bernit1213@gmail.com>
2023-11-23 11:11:12 +0800

    10

--- hack2/src/app/projects/[projectId]/actions.ts
+++ hack2/src/app/projects/[projectId]/actions.ts
@@ -20,7 +20,7 @@ export async function getProject(projectId: string) {
 
   const userToProject = await db.query.usersToProjectsTable.findFirst({
     // TODO: 8. Select the correct project by userId and projectId
-    
+
     // TODO: 8. end
     columns: {},
     with: {
@@ -119,8 +119,8 @@ export async function deleteTask(taskId: string, projectId: string) {
 
   // TODO: 10. Delete the task whose displayId is `taskId`
   await db
-    .delete(tasksTable)
-    .where(eq(tasksTable.displayId, taskId));
+  .delete(tasksTable)
+  .where(eq(tasksTable.displayId, taskId));
   // TODO: 10. end
 
   revalidatePath(`/projects/${projectId}`);
