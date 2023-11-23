77210ecba7bfeb3a9ad626dde98af2106999d2e9
Bernit-Chen <bernit1213@gmail.com>
2023-11-23 09:43:01 +0800

    1

--- hack2/src/db/index.ts
+++ hack2/src/db/index.ts
@@ -6,8 +6,9 @@ import { privateEnv } from "@/lib/env/private";
 import * as schema from "./schema";
 
 const client = new Client({
+
   // TODO: 1.1 Add your private environment variables here for your database (postgres)
-  connectionString: "<connection_string>", // change this line
+  connectionString: privateEnv.POSTGRES_URL, // change this line
   // TODO: 1.1 end
 
   connectionTimeoutMillis: 5000,
